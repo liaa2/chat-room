cccffdee37b0ac75416fc3471165f17447d2d566
liaa2 <cello1412@gmail.com>
2020-07-27 21:35:37 +1000

    add controller boilerplates

---
+++ controllers/delete.js
@@ -0,0 +1,7 @@
+const deleteRoomById = async (req, res) => {};
+const deleteMessageById = async (req, res) => {};
+
+module.exports = {
+  deleteRoomById,
+  deleteMessageById,
+};
---
+++ controllers/user.js
@@ -0,0 +1,11 @@
+const getAllUsers = async (req, res) => {};
+const getUserById = async (req, res) => {};
+const createUser = async (req, res) => {};
+const deleteUserById = async (req, res) => {};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  deleteUserById,
+};
